a159b11539f1427197becc4b00f78aa6629e2bc3
ljl <776745393@qq.com>
2022-02-24 13:32:58 +0800

    add koa-jwt

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,12 @@
 const Koa = require("koa");
 const KoaBody = require("koa-body");
+const jwt = require("koa-jwt");
 const router = require("../route/index");
 require("../db");
 
 const app = new Koa(); // 创建koa应用
 
+app.use(jwt({ secret: "shared-secret" }).unless({ path: [/^\/users/] }));
 app.use(KoaBody());
 app.use(router.routes());
 app.use(
